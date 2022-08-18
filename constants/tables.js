16ac310da427fd8793ba9f57b67a0e2d8d5749e2
Anoop Kumar Sharma <anoop-sharma@users.noreply.github.com>
2022-08-18 17:50:12 +0530

    Base project

---
+++ constants/tables.js
@@ -0,0 +1,9 @@
+/*
+    Project Tables
+    Created : 2022
+*/
+
+// Exports ------------- Needed utmost as without these nothing will work
+module.exports = {
+    admin: 'admin'
+}
---
+++ migration/seeds.sql
@@ -0,0 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 20, 2022 at 07:03 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `samplenode`
+--
+CREATE DATABASE IF NOT EXISTS `samplenode` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `samplenode`;
+
+--
+-- Dumping data for table `admin`
+--
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
